c164811364fe651fd227f08e08ed46f0a0b5ac69
lokytech5 <lokytech5@gmail.com>
2023-09-29 03:53:44 +0100

    Initial commit

---
+++ src/main/java/com/lokytech/FocusBooker/FocusBookerApplication.java
@@ -0,0 +1,13 @@
+package com.lokytech.FocusBooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusBookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokytech/FocusBooker/FocusBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokytech.FocusBooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
