0fcd393c432dbca670079bc339d74b3c56b2c47a
lokytech5 <lokytech5@gmail.com>
2023-09-29 19:50:09 +0100

    Added bookingDTO class and updated BookingService class

---
+++ src/main/java/com/lokytech/FocusBooker/dto/BookingDTO.java
@@ -0,0 +1,97 @@
+package com.lokytech.FocusBooker.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class BookingDTO {
+    private UsersDTO usersDTO;
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String serviceType;
+    private LocalDate date;
+    private LocalTime time;
+    private String message;
+    private Long userId;
+
+    public UsersDTO getUsersDTO() {
+        return usersDTO;
+    }
+
+    public void setUsersDTO(UsersDTO usersDTO) {
+        this.usersDTO = usersDTO;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
--- src/main/java/com/lokytech/FocusBooker/service/BookingService.java
+++ src/main/java/com/lokytech/FocusBooker/service/BookingService.java
@@ -1,4 +1,28 @@
 package com.lokytech.FocusBooker.service;
 
+import com.lokytech.FocusBooker.repository.BookingRepository;
+import jakarta.validation.Validator;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class BookingService {
+
+    private BookingRepository bookingRepository;
+
+    private UsersService usersService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private Validator validator;
+
+    @Autowired
+    public BookingService(BookingRepository bookingRepository, UsersService usersService) {
+        this.bookingRepository = bookingRepository;
+        this.usersService = usersService;
+    }
+
+
+
 }
