668b727b0ecac534dc69d3c28650dd973e5ba061
lokytech5 <lokytech5@gmail.com>
2023-09-29 04:22:40 +0100

    Added Users Repository class with Jpa and service class for business logic

--- src/main/java/com/lokytech/FocusBooker/repository/UsersRepository.java
+++ src/main/java/com/lokytech/FocusBooker/repository/UsersRepository.java
@@ -1,4 +1,7 @@
 package com.lokytech.FocusBooker.repository;
 
-public class UsersRepository {
+import com.lokytech.FocusBooker.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
 }
--- src/main/java/com/lokytech/FocusBooker/service/UsersService.java
+++ src/main/java/com/lokytech/FocusBooker/service/UsersService.java
@@ -1,4 +1,9 @@
 package com.lokytech.FocusBooker.service;
 
+import com.lokytech.FocusBooker.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UsersService {
+    @Autowired
+    private UsersRepository usersRepository;
 }
