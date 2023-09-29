5a3dd773becfba42ab11afff72e7533b92a930b9
lokytech5 <lokytech5@gmail.com>
2023-09-29 19:55:27 +0100

    updated BookingService class by adding save, findAllBooking and findBookingById methods

--- src/main/java/com/lokytech/FocusBooker/service/BookingService.java
+++ src/main/java/com/lokytech/FocusBooker/service/BookingService.java
@@ -1,9 +1,16 @@
 package com.lokytech.FocusBooker.service;
 
+import com.lokytech.FocusBooker.dto.BookingDTO;
+import com.lokytech.FocusBooker.dto.UsersDTO;
+import com.lokytech.FocusBooker.entity.Booking;
+import com.lokytech.FocusBooker.entity.Users;
 import com.lokytech.FocusBooker.repository.BookingRepository;
 import jakarta.validation.Validator;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
 
 public class BookingService {
 
@@ -22,6 +29,31 @@ public class BookingService {
         this.bookingRepository = bookingRepository;
         this.usersService = usersService;
     }
+
+    public List<Booking> findAllBooking(){
+        return bookingRepository.findAll();
+    }
+
+    public Optional<Booking> findBookingById(Long id){
+        return bookingRepository.findById(id);
+    }
+
+    public BookingDTO saveBooking(Long userId, Booking booking){
+        Users user = usersService.findUserById(userId);
+        booking.setUser(user);
+
+        Booking savedBooking = bookingRepository.save(booking);
+        BookingDTO dto = modelMapper.map(savedBooking, BookingDTO.class);
+
+        if (user != null) {
+            UsersDTO userDTO = modelMapper.map(user, UsersDTO.class);
+            dto.setUsersDTO(userDTO);
+        }
+
+        return dto;
+    }
+
+
 
 
 
